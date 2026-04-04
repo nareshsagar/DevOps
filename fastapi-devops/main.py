f441db7d3f454fc02ca59f779672d10b2564f8a2
nareshsagar <51433983+nareshsagar@users.noreply.github.com>
2026-04-04 19:20:14 +0530

    Revert "Naresh"

--- fastapi-devops/main.py
+++ fastapi-devops/main.py
@@ -4,11 +4,9 @@ app = FastAPI()
 
 @app.get("/")
 def read_root():
-    # We keep the cleanest version of your message
     return {"message": "Hello Naresh! Your FastAPI CI/CD Pipeline is 2.0V"}
 
 @app.get("/health")
 def status():
     return {"status": "ok"}
-  
-  
+
